34aec0a8dc0497535186241543d85d6a2221e01f
ceholmes <chuck.holmes@gmail.com>
2014-04-11 18:14:22 -0400

    express config

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(express.logger('dev'));
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -30,6 +30,12 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+
+app.post('/', function(request, response){
+  console.log(request.body);      // your JSON
+  response.send(request.body);    // echo the result back
+});
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
