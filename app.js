a32b60231eccc9767b361d12bf247f3fd047b4e8
ceholmes <chuck.holmes@gmail.com>
2014-04-18 15:52:49 -0400

    Updated to Express 4.0

--- app.js
+++ app.js
@@ -1,41 +1,45 @@
-/**
- * Module dependencies.
- */
 
+// module dependencies.
 var express = require('express');
-var routes = require('./routes');
-var render = require('./routes/render');
 var http = require('http');
 var path = require('path');
-var exphbs = require('express3-handlebars');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var handlebars = require('express3-handlebars');
+var routes = require('./routes');
+var render = require('./routes/render');
 
+// create app
 var app = express();
-
-// all environments
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-
 app.set('port', process.env.PORT || 3000);
+
+// view engine
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.logger('dev'));
-app.use(express.methodOverride());
-app.use(app.router);
+// middleware
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+//if ('development' == app.get('env')) {
+  //app.use(express.errorHandler());
+//}
 
+// routes
 app.get('/', routes.index);
-
 app.get('/render/url', render.url);
 app.post('/render/html', render.html);
 
-
+// create server
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
