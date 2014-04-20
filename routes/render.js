c6edff7df2f064bb0c4eca77d3f64f939f085f49
ceholmes <chuck.holmes@gmail.com>
2014-04-20 16:04:24 -0400

    Added RenderUrl

--- routes/render.js
+++ routes/render.js
@@ -2,11 +2,36 @@
 // GET render/url
 exports.url = function(req, res){      
     
-    var url = req.param('url');
-    var file = req.param('file');
+    // dependencies
+    var path = require('path');
+    var childProcess = require('child_process');
+    
+    // set paths    
+    var url = 'http://' + req.param('url');        
+    var file = path.normalize(__dirname + '/../public/images/' + req.param('file'));        
+    var script = path.join(__dirname, 'render.phantom.js');
+    
+    // options                                    
+    var width = req.param('width') || 1680;
+    var height = req.param('height') || 1050;
+    var delay = req.param('delay') || 1000;
+
+    
+    console.log('url:' + url + ', file:' + file + ', width:' + width + ', height:' + height + ', delay:' + delay);    
+    
+    // args
+    var args = [script, url, file, width, height, delay];
+
+    // exec process    
+    childProcess.execFile('phantomjs', args, function(err, stdout, stderr) {
+        if (err !== null) 
+            console.log('error:' + err);
+        else        
+            console.log(stdout);      
+        
+        res.sendfile(file);
+    });    
             
-    console.log('url=' + url + ', file=' + file);
-    res.send('url=' + url + ', file=' + file);    
 };
 
 
